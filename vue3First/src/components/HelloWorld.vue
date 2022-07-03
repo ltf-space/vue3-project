03a7f5fb5b494d2eb0acd109a1d950c6c6458d18
ltf-space <1036285771@qq.com>
2022-07-03 16:56:39 +0800

    日常练习

---
+++ vue3First/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>{{count}}</div>
+  <button @click="handleCount">增加count</button>
+  <div>{{obj.name}}</div>
+  <div>{{arr1[0]}}</div>
+</template>
+
+<script>
+import { ref,reactive } from 'vue'
+export default {
+  setup(props) {
+    const count = ref(0)
+    function handleCount(){
+      // count是一个对象，值在value里
+      count.value++
+    }
+    // 响应式对象
+    let obj = reactive({
+      name:'zhansan',
+      age:'20',
+      sex:'男'
+    })
+    let arr1 = ref(['lisi','zhansan'])
+    // arr1.push('lisi')
+    return {count,handleCount,obj,arr1}
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ vue3First/src/views/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{count}}</div>
+</template>
+
+<script setup>
+ import { ref } from 'vue'
+ defineProps({
+  msg: String
+ })
+ let count = ref(0)
+</script>
+<style scoped>
+
+</style>
